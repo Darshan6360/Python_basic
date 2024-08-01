051a0945b34f3a2bd7f24582d67a311e593b2e08
Darshan6360 <125114494+Darshan6360@users.noreply.github.com>
2024-08-01 15:05:07 +0530

    Add files via upload

---
+++ types_of_operators.py
@@ -0,0 +1,49 @@
+""" #1) arithmetic operators
+
+a = 5
+b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)  #remainder
+print(a ** b) #power operator 
+
+#2) Relational operators
+
+a = 50
+b = 20
+print(a == b) #False
+print(a != b) #True
+print(a > b)  #True
+print(a < b)  #False
+print(a >= b) #True
+print(a <= b) #False 
+
+#3) Assignment operators
+
+num = 10
+#num += 10
+#(num = num + 10 #10+10 => 20)
+#num -= 10
+#num *= 10
+#num /= 5
+#num %= 3
+num **= 10
+print("num :", num) """
+
+#4) Logical operators
+
+a = 50
+b = 30
+print(not False)
+print(not (a > b))
+
+val1 = True
+val2 = False
+print("And operator:", val1 and val2)
+print("Or operator:", val1 or val2)
+print("Or operator:", (a == b) or (a > b))
+
+
+
