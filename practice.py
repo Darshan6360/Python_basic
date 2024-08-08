8adaa9133ecb11cc4b42f974ade18833117fce96
Darshan6360 <125114494+Darshan6360@users.noreply.github.com>
2024-08-08 19:06:39 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,24 @@
+"""# write a program to input 2 numbers & print their sum
+
+a = int(input("enter a value:"))
+b = int(input("enter b value:"))
+sum = (a + b)
+print("Sum of the 2 numbers:", sum) 
+
+# write a program to input side of a square & print its area (area = side*side)
+
+side = float(input("enter square side:"))
+print("area =", side * side) 
+
+# wap to input 2 floating point numbers & print their average
+A = float(input("enter A value:"))
+B = float(input("enter B value:"))
+Avg = (A + B)/2
+print("Average of 2 numbers:", Avg) """
+
+#wap to input 2 int numbers, a and b. Print True if a is greater than or equal to b. if not print False
+
+a = int(input("enter a value:"))
+b = int(input("enter b value:"))
+print( a>= b )
+
